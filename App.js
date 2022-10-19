4f5fc4a89ee7d1a7d48dc23aaa6a1ca74efa6bcd
George Baez <g.baez@gbh.com.do>
2022-10-19 18:26:30 -0400

    Start creating PickPlayer component

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from "react";
+import Main from './src/components/Main.jsx';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Main />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
